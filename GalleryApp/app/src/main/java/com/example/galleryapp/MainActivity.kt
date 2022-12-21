c5359e732a8be9b9bb3e275a267458e6943cb86a
Peter <rawiwatpan@gmail.com>
2022-12-21 11:09:21 +0700

    image added

--- GalleryApp/app/src/main/java/com/example/galleryapp/MainActivity.kt
+++ GalleryApp/app/src/main/java/com/example/galleryapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.galleryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.firework)
+        imageView.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.firework_background))
+        val imageView2 = findViewById<ImageView>(R.id.shoes)
+        imageView.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.Shoes_background))
     }
 }
